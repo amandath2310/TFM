16c7461b68d975889be3388e71d03bca40959b04
amandath2310 <amandath2310@gmail.com>
2024-04-10 02:14:33 +0200

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-#Run each input file generated using orca
-for i in range(1,20):
-    input_file = f"{i}ccsd.inp"
-    output_file = f"{i}ccsd.out"
-    command = f"orca {input_file} > {output_file}"
-    subprocess.run(command,shell=True)
